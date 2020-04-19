ff765ee3db2ef1550edd3d74b7ee161f75f1d895
Eduardo Gomez <edugof@users.noreply.github.com>
2020-04-19 11:56:10 +0200

    Use environment variables properly

--- airquality/airquality.py
+++ airquality/airquality.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
-import serial, time
+import serial, time, os
 from Adafruit_IO import Client
-aio = Client($ADAUSER, $ADAKEY)
+aio = Client(os.environ.get('IO_USERNAME'), os.environ.get('IO_KEY'))
 
 ser = serial.Serial('/dev/ttyUSB0')
 
 while True:
-	data = []
-	for index in range (0,10):
-		datum = ser.read()
-		data.append(datum)
+        data = []
+        for index in range (0,10):
+                datum = ser.read()
+                data.append(datum)
 
-	pmtwofive = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
-	aio.send('airquality25pm', pmtwofive)
-	pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
-	aio.send('airquality10pm', pmten)
-	time.sleep(10)
+        pmtwofive = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
+        aio.send('airquality25pm', pmtwofive)
+        pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
+        aio.send('airquality10pm', pmten)
+        time.sleep(10)
